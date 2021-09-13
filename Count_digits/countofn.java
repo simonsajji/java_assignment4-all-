61e2be4e34dc70e692c2c578cac375a3f6fac031
simon mathews <simonmathai777@gmail.com>
2021-09-13 16:56:31 +0530

    3rd commit Count-digitd

---
+++ Count_digits/countofn.java
@@ -0,0 +1,12 @@
+package Count_digits;
+
+public class countofn {
+	public static void main(String args[]) {
+		int[] digits= {9,45,23,10,5,6,7,8,3,5,3,5,244,24,22,75,34,21,564,354,23,12,435,3412,443,66,3};
+		int count=digits.length;
+		System.out.println("the count of N digits is "+count);
+		
+		
+	}
+
+}
